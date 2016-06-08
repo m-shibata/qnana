2e9367942965bc94f3726ed8fc0e64341a1846d0
Mitsuya Shibata <mty.shibata@gmail.com>
2016-06-08 19:33:37 +0900

    dump touch plane

--- battle_air.go
+++ battle_air.go
@@ -8,35 +8,40 @@ import (
 type SeikuId int
 
 func (i SeikuId) String() string {
-        switch i {
-        case SeikuIdTie:
-            return "均衡"
-        case SeikuIdAcquire:
-            return "確保"
-        case SeikuIdMastery:
-            return "優勢"
-        case SeikuIdBackfoot:
-            return "劣勢"
-        case SeikuIdLost:
-            return "喪失"
-        }
-        return strconv.Itoa(int(i))
+	switch i {
+	case 0:
+		return "均衡"
+	case 1:
+		return "確保"
+	case 2:
+		return "優勢"
+	case 3:
+		return "劣勢"
+	case 4:
+		return "喪失"
+	}
+	return strconv.Itoa(int(i))
 }
 
-const (
-        SeikuIdTie SeikuId = iota
-        SeikuIdAcquire
-        SeikuIdMastery
-        SeikuIdBackfoot
-        SeikuIdLost
-)
+type TouchPlane int
+
+func (i TouchPlane) String() string {
+	if i != -1 {
+		if len(equipData) > int(i) {
+			return equipData[int(i)].ApiName
+		}
+		return "OK"
+	}
+	return "NG"
+}
 
 type Stage1 struct {
-	ApiDispSeiku  SeikuId `json:"api_disp_seiku"`
-	ApiECount     int `json:"api_e_count"`
-	ApiELostcount int `json:"api_e_lostcount"`
-	ApiFCount     int `json:"api_f_count"`
-	ApiFLostcount int `json:"api_f_lostcount"`
+	ApiDispSeiku  SeikuId      `json:"api_disp_seiku"`
+	ApiECount     int          `json:"api_e_count"`
+	ApiELostcount int          `json:"api_e_lostcount"`
+	ApiFCount     int          `json:"api_f_count"`
+	ApiFLostcount int          `json:"api_f_lostcount"`
+	ApiTouchPlane []TouchPlane `json:"api_touch_plane"`
 }
 
 type Stage2 struct {
@@ -59,13 +64,15 @@ type Kouku struct {
 
 func (kouku Kouku) calcKoukuDamage(label string, hps1 []int, hps2 []int) {
 	fmt.Printf("[%7s0]: %-5s", label, kouku.ApiStage1.ApiDispSeiku)
-	fmt.Printf("%10s / %10s\n", "All", "Bombers")
-	fmt.Printf("            Friend %3d => %3d / %3d => %3d\n",
+	fmt.Printf("%10s / %10s / Touch\n", "All", "Bombers")
+	fmt.Printf("            Friend %3d => %3d / %3d => %3d / %s\n",
 		kouku.ApiStage1.ApiFCount, kouku.ApiStage1.ApiFCount-kouku.ApiStage1.ApiFLostcount,
-		kouku.ApiStage2.ApiFCount, kouku.ApiStage2.ApiFCount-kouku.ApiStage2.ApiFLostcount)
-	fmt.Printf("            Enemy  %3d => %3d / %3d => %3d\n",
+		kouku.ApiStage2.ApiFCount, kouku.ApiStage2.ApiFCount-kouku.ApiStage2.ApiFLostcount,
+		kouku.ApiStage1.ApiTouchPlane[0])
+	fmt.Printf("            Enemy  %3d => %3d / %3d => %3d / %s\n",
 		kouku.ApiStage1.ApiECount, kouku.ApiStage1.ApiECount-kouku.ApiStage1.ApiELostcount,
-		kouku.ApiStage2.ApiECount, kouku.ApiStage2.ApiECount-kouku.ApiStage2.ApiELostcount)
+		kouku.ApiStage2.ApiECount, kouku.ApiStage2.ApiECount-kouku.ApiStage2.ApiELostcount,
+		kouku.ApiStage1.ApiTouchPlane[1])
 	fmt.Printf("[%7s1]:", label)
 	for i, v := range kouku.ApiStage3.ApiFdam[1:] {
 		hps1[i] -= int(v)
@@ -82,4 +89,3 @@ func (kouku Kouku) calcKoukuDamage(label string, hps1 []int, hps2 []int) {
 	}
 	fmt.Printf("\n")
 }
-
